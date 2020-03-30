8262c5014a637101343f77be477a2c96381f70aa
Alexander Teusz <teusz.alexander@gmail.com>
2020-03-30 10:23:15 +0200

    use dropdownmenu without underline

--- lib/screens/home/widgets/searchDropDown.dart
+++ lib/screens/home/widgets/searchDropDown.dart
@@ -42,21 +42,23 @@ class _SearchDropDownState extends State<SearchDropDown> {
                 color: Colors.grey, style: BorderStyle.solid, width: 0.80),
           ),
           child: 
-            DropdownButton<String>(
-              items: this.dropdownValues
-                .map((value) => DropdownMenuItem(
-                        child: Text(value),
-                        value: value,
-                      ))
-                .toList(),
-              onChanged: (String value) {
-                setState(() {
-                  _value = value;
-                });
-              },
-              value: _value,
-              hint: Text(this.placeholder),
-          ),
+            DropdownButtonHideUnderline(
+              child:  DropdownButton<String>(
+                items: this.dropdownValues
+                  .map((value) => DropdownMenuItem(
+                          child: Text(value),
+                          value: value,
+                        ))
+                  .toList(),
+                onChanged: (String value) {
+                  setState(() {
+                    _value = value;
+                  });
+                },
+                value: _value,
+                hint: Text(this.placeholder),
+              ),
+            )
         )
     );
   }
