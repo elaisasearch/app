ebb0893395c2b1812d7245bb0c234ce0f6859a94
Alexander Teusz <teusz.alexander@gmail.com>
2020-03-30 16:28:22 +0200

    added container layout to divide the drawer into top and bottom part

--- lib/screens/main/widgets/mainDrawer.dart
+++ lib/screens/main/widgets/mainDrawer.dart
@@ -6,40 +6,68 @@ class MainDrawer extends StatelessWidget {
   Widget build(BuildContext context) {
 
     return Drawer(
-      child: ListView(
-        padding: EdgeInsets.zero,
+      child: Column(
+
         children: <Widget>[
           DrawerHeader(
-            decoration: BoxDecoration(
-              color: Colors.grey
-            ),
             child: Column(
               children: <Widget>[
                 Image(
                   image: AssetImage('assets/images/logo.png'), 
-                  width: 50
+                  width: 150
                 ),
-                Text('Menu')
+                Text(
+                  'Menu',
+                  style: TextStyle(fontSize: 20),
+                )
               ],
             ),
           ),
-          ListTile(
-            leading: Icon(Icons.school),
-            title: Text('Language Tests')
+          Expanded(
+            child: ListView(
+              children: <Widget>[
+                ListTile(
+                  leading: Icon(Icons.school),
+                  title: Text('Language Tests')
+                ),
+              ],
+            )
           ),
-          Divider(),
-          ListTile(
-            leading: Icon(Icons.help),
-            title: Text('Help')
-          ),
-          Divider(),
-          ListTile(
-            leading: Icon(Icons.code),
-            title: Text('See Code')
-          ),
-          ListTile(
-            leading: Icon(Icons.bug_report),
-            title: Text('Report Bug')
+          Container(
+            child: Align(
+              alignment: FractionalOffset.bottomCenter,
+              child: Container(
+                child: Column(
+                  children: <Widget>[
+                    Divider(),
+                    ListTile(
+                      leading: Icon(Icons.help),
+                      title: Text('Help')
+                    ),
+                    Divider(),
+                    ListTile(
+                      leading: Icon(Icons.code),
+                      title: Text('See Code')
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.bug_report),
+                      title: Text('Report Bug')
+                    ),
+                    Divider(thickness: 1),
+                    Container(
+                      padding: EdgeInsets.all(10),
+                      height: 50,
+                      child: Text(
+                        'v1.0.0 - elaisa.org',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold
+                        )
+                      )
+                    )
+                  ],
+                )
+              )
+            )
           )
         ],
       ),
