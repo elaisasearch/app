0dd90bf351b828cb85e05c36e7cf8395a58866ff
Alexander Teusz <teusz.alexander@gmail.com>
2020-03-31 21:55:06 +0200

    put provider to a higher level

--- lib/main.dart
+++ lib/main.dart
@@ -7,22 +7,18 @@ import 'package:app/providers/mainProvider.dart';
 // import widgets
 import 'package:app/screens/main/main-screen.dart';
 
-
-void main() => runApp(ElaisaApp());
+void main() => runApp(ChangeNotifierProvider<MainState>(
+    create: (_) => MainState(), child: ElaisaApp()));
 
 class ElaisaApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "Elaisa Search", 
-      theme: ThemeData(
-        primaryColor: Colors.white,
-      ),
-      home: ChangeNotifierProvider<MainState>(
-        create: (_) => MainState(),
-        child: MainScreen()
-      )
-    );
+        debugShowCheckedModeBanner: false,
+        title: "Elaisa Search",
+        theme: ThemeData(
+          primaryColor: Colors.white,
+        ),
+        home: MainScreen());
   }
 }
