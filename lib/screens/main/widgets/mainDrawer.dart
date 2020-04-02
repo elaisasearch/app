ba361869004a7c6d108a25d11b93c662ddd02708
Alexander Teusz <teusz.alexander@gmail.com>
2020-04-02 23:53:52 +0200

    added oval clipper to mainDrawer

--- lib/screens/main/widgets/mainDrawer.dart
+++ lib/screens/main/widgets/mainDrawer.dart
@@ -1,11 +1,10 @@
+import 'package:app/screens/main/widgets/ovalRightClipper.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class MainDrawer extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-
     // method to open a url
     _launchUrl(String url) async {
       if (await canLaunch(url)) {
@@ -15,18 +14,17 @@ class MainDrawer extends StatelessWidget {
       }
     }
 
-    return Drawer(
-      child: Container(
-        color: Colors.white,
-        child:Column(
+    return ClipPath(
+        clipper: OvalRightBorderClipper(),
+        child: Drawer(
+            child: Container(
+      color: Colors.white,
+      child: Column(
         children: <Widget>[
           DrawerHeader(
             child: Column(
               children: <Widget>[
-                Image(
-                  image: AssetImage('assets/images/logo.png'), 
-                  width: 150
-                ),
+                Image(image: AssetImage('assets/images/logo.png'), width: 150),
                 Text(
                   'Menu',
                   style: TextStyle(fontSize: 20),
@@ -35,57 +33,49 @@ class MainDrawer extends StatelessWidget {
             ),
           ),
           Expanded(
-            child: ListView(
-              padding: EdgeInsets.zero,
-              children: <Widget>[
-                ListTile(
-                  leading: Icon(Icons.school),
-                  title: Text('Language Tests'),
-                ),
-              ],
-            )
-          ),
+              child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              ListTile(
+                leading: Icon(Icons.school),
+                title: Text('Language Tests'),
+              ),
+            ],
+          )),
           Container(
-            child: Align(
-              alignment: FractionalOffset.bottomCenter,
-              child: Container(
-                child: Column(
-                  children: <Widget>[
-                    Divider(),
-                    ListTile(
-                      leading: Icon(Icons.help),
-                      title: Text('Help'),
-                      onTap: () => _launchUrl('https://github.com/elaisasearch/elaisa.org/blob/master/README.md')
-                    ),
-                    Divider(),
-                    ListTile(
-                      leading: Icon(Icons.code),
-                      title: Text('See Code'),
-                      onTap: () => _launchUrl('https://github.com/elaisasearch/app')
-                    ),
-                    ListTile(
-                      leading: Icon(Icons.bug_report),
-                      title: Text('Report Bug'),
-                      onTap: () => _launchUrl('https://github.com/elaisasearch/app/issues'),
-                    ),
-                    Divider(thickness: 1),
-                    Container(
-                      padding: EdgeInsets.all(10),
-                      height: 50,
-                      child: Text(
-                        'v1.0.0 - elaisa.org',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold
-                        )
-                      )
-                    )
-                  ],
-                )
-              )
-            )
-          )
+              child: Align(
+                  alignment: FractionalOffset.bottomCenter,
+                  child: Container(
+                      child: Column(
+                    children: <Widget>[
+                      Divider(),
+                      ListTile(
+                          leading: Icon(Icons.help),
+                          title: Text('Help'),
+                          onTap: () => _launchUrl(
+                              'https://github.com/elaisasearch/elaisa.org/blob/master/README.md')),
+                      Divider(),
+                      ListTile(
+                          leading: Icon(Icons.code),
+                          title: Text('See Code'),
+                          onTap: () => _launchUrl(
+                              'https://github.com/elaisasearch/app')),
+                      ListTile(
+                        leading: Icon(Icons.bug_report),
+                        title: Text('Report Bug'),
+                        onTap: () => _launchUrl(
+                            'https://github.com/elaisasearch/app/issues'),
+                      ),
+                      Divider(thickness: 1),
+                      Container(
+                          padding: EdgeInsets.all(10),
+                          height: 50,
+                          child: Text('v1.0.0 - elaisa.org',
+                              style: TextStyle(fontWeight: FontWeight.bold)))
+                    ],
+                  ))))
         ],
       ),
-    ));
+    )));
   }
 }
