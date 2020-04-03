3fd95c062cde46e0721471522ab7183fd98a4c78
Alexander Teusz <teusz.alexander@gmail.com>
2020-04-03 17:16:10 +0200

    handle result item error. return a container if the result cannot be built

--- lib/screens/results/widgets/resultListItem.dart
+++ lib/screens/results/widgets/resultListItem.dart
@@ -117,113 +117,117 @@ class _ResultListItemState extends State<ResultListItem> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-        margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
-        shape:
-            RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
-        child: Padding(
-            padding: const EdgeInsets.all(15.0),
-            child: Column(
-                // align content left
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    '${widget.url.substring(0, 30)}...',
-                    maxLines: 1,
-                    style: TextStyle(
-                      fontSize: 10,
-                      color: Colors.green[600],
+    try {
+      return Card(
+          margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+          child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Column(
+                  // align content left
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      '${widget.url.substring(0, 30)}...',
+                      maxLines: 1,
+                      style: TextStyle(
+                        fontSize: 10,
+                        color: Colors.green[600],
+                      ),
                     ),
-                  ),
-                  Container(
-                      margin: const EdgeInsets.only(top: 10.0),
-                      child: GestureDetector(
-                          onTap: () {
-                            _launchUrl(widget.url);
-                          },
-                          child: Text(
-                            widget.title,
+                    Container(
+                        margin: const EdgeInsets.only(top: 10.0),
+                        child: GestureDetector(
+                            onTap: () {
+                              _launchUrl(widget.url);
+                            },
+                            child: Text(
+                              widget.title,
+                              style: TextStyle(
+                                  fontSize: 13,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.blueAccent),
+                            ))),
+                    Container(
+                        margin: const EdgeInsets.only(top: 10.0),
+                        child: Text(widget.meta['desc'],
                             style: TextStyle(
-                                fontSize: 13,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.blueAccent),
-                          ))),
-                  Container(
-                      margin: const EdgeInsets.only(top: 10.0),
-                      child: Text(widget.meta['desc'],
-                          style: TextStyle(
-                              color: Colors.grey[700], fontSize: 12))),
-                  Container(
-                      margin: const EdgeInsets.only(top: 10.0),
-                      child: ExpansionTile(
-                        trailing: Icon(
-                          Icons.insert_chart,
-                          color: Colors.grey[700],
-                        ),
-                        title: null,
-                        leading: IconButton(
-                          icon: Icon(
-                            this.isMarked
-                                ? Icons.bookmark
-                                : Icons.bookmark_border,
+                                color: Colors.grey[700], fontSize: 12))),
+                    Container(
+                        margin: const EdgeInsets.only(top: 10.0),
+                        child: ExpansionTile(
+                          trailing: Icon(
+                            Icons.insert_chart,
                             color: Colors.grey[700],
                           ),
-                          onPressed: () {
-                            handleBookmarkTapped();
-                          },
-                        ),
-                        children: <Widget>[
-                          Container(
-                              padding: new EdgeInsets.all(10.0),
-                              child: Column(children: <Widget>[
-                                Row(
-                                  children: <Widget>[
-                                    Text('Language Level: ',
-                                        style: TextStyle(
-                                            fontWeight: FontWeight.bold)),
-                                    Text(widget.level)
-                                  ],
-                                ),
-                                Container(
-                                    padding: const EdgeInsets.only(top: 15.0),
-                                    alignment: Alignment.topLeft,
-                                    child: Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: <Widget>[
-                                        Text('Language Level distribution: ',
-                                            style: TextStyle(
-                                                fontWeight: FontWeight.bold)),
-                                        Padding(
-                                            padding: new EdgeInsets.only(
-                                                top: 10.0, bottom: 10.0),
-                                            child: Text(
-                                                "A1:\t\t ${widget.levelMeta['A1']}%,\nA2:\t\t${widget.levelMeta['A2']}%,\nB1:\t\t ${widget.levelMeta['B1']}%,\nB2:\t\t${widget.levelMeta['B2']}%,\nC1:\t\t ${widget.levelMeta['C1']}%,\nC2:\t\t${widget.levelMeta['C2']}%")),
-                                        Row(
-                                          children: <Widget>[
-                                            Text('Word Length: ',
-                                                style: TextStyle(
-                                                    fontWeight:
-                                                        FontWeight.bold)),
-                                            Text(
-                                              // capitalize first letter of difficulty
-                                              // Returns: Easy | Hard
-                                              '${widget.levelMeta['difficulty'][0].toUpperCase()}${widget.levelMeta['difficulty'].substring(1)}',
+                          title: null,
+                          leading: IconButton(
+                            icon: Icon(
+                              this.isMarked
+                                  ? Icons.bookmark
+                                  : Icons.bookmark_border,
+                              color: Colors.grey[700],
+                            ),
+                            onPressed: () {
+                              handleBookmarkTapped();
+                            },
+                          ),
+                          children: <Widget>[
+                            Container(
+                                padding: new EdgeInsets.all(10.0),
+                                child: Column(children: <Widget>[
+                                  Row(
+                                    children: <Widget>[
+                                      Text('Language Level: ',
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.bold)),
+                                      Text(widget.level)
+                                    ],
+                                  ),
+                                  Container(
+                                      padding: const EdgeInsets.only(top: 15.0),
+                                      alignment: Alignment.topLeft,
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: <Widget>[
+                                          Text('Language Level distribution: ',
                                               style: TextStyle(
-                                                  fontWeight: FontWeight.bold,
-                                                  color: widget.levelMeta[
-                                                              'difficulty'] ==
-                                                          'easy'
-                                                      ? Colors.green
-                                                      : Colors.red),
-                                            )
-                                          ],
-                                        ),
-                                      ],
-                                    ))
-                              ]))
-                        ],
-                      ))
-                ])));
+                                                  fontWeight: FontWeight.bold)),
+                                          Padding(
+                                              padding: new EdgeInsets.only(
+                                                  top: 10.0, bottom: 10.0),
+                                              child: Text(
+                                                  "A1:\t\t ${widget.levelMeta['A1']}%,\nA2:\t\t${widget.levelMeta['A2']}%,\nB1:\t\t ${widget.levelMeta['B1']}%,\nB2:\t\t${widget.levelMeta['B2']}%,\nC1:\t\t ${widget.levelMeta['C1']}%,\nC2:\t\t${widget.levelMeta['C2']}%")),
+                                          Row(
+                                            children: <Widget>[
+                                              Text('Word Length: ',
+                                                  style: TextStyle(
+                                                      fontWeight:
+                                                          FontWeight.bold)),
+                                              Text(
+                                                // capitalize first letter of difficulty
+                                                // Returns: Easy | Hard
+                                                '${widget.levelMeta['difficulty'][0].toUpperCase()}${widget.levelMeta['difficulty'].substring(1)}',
+                                                style: TextStyle(
+                                                    fontWeight: FontWeight.bold,
+                                                    color: widget.levelMeta[
+                                                                'difficulty'] ==
+                                                            'easy'
+                                                        ? Colors.green
+                                                        : Colors.red),
+                                              )
+                                            ],
+                                          ),
+                                        ],
+                                      ))
+                                ]))
+                          ],
+                        ))
+                  ])));
+    } catch (error) {
+      return Container();
+    }
   }
 }
