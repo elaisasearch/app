6bc2fc9d7f0fbef85833d1fea4b0524f48ce42f6
Alexander Teusz <teusz.alexander@gmail.com>
2020-04-03 17:34:42 +0200

    new design for result item

--- lib/screens/results/widgets/resultListItem.dart
+++ lib/screens/results/widgets/resultListItem.dart
@@ -122,6 +122,7 @@ class _ResultListItemState extends State<ResultListItem> {
           margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
           shape:
               RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+              elevation: 5,
           child: Padding(
               padding: const EdgeInsets.all(15.0),
               child: Column(
@@ -133,7 +134,7 @@ class _ResultListItemState extends State<ResultListItem> {
                       maxLines: 1,
                       style: TextStyle(
                         fontSize: 10,
-                        color: Colors.green[600],
+                        color: Colors.black,
                       ),
                     ),
                     Container(
@@ -145,21 +146,20 @@ class _ResultListItemState extends State<ResultListItem> {
                             child: Text(
                               widget.title,
                               style: TextStyle(
-                                  fontSize: 13,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.blueAccent),
+                                  fontSize: 15,
+                                  color: Colors.blue[800]),
                             ))),
                     Container(
                         margin: const EdgeInsets.only(top: 10.0),
                         child: Text(widget.meta['desc'],
                             style: TextStyle(
-                                color: Colors.grey[700], fontSize: 12))),
+                                color: Colors.black45, fontSize: 12))),
                     Container(
                         margin: const EdgeInsets.only(top: 10.0),
                         child: ExpansionTile(
                           trailing: Icon(
                             Icons.insert_chart,
-                            color: Colors.grey[700],
+                            color: Colors.black54,
                           ),
                           title: null,
                           leading: IconButton(
@@ -167,7 +167,7 @@ class _ResultListItemState extends State<ResultListItem> {
                               this.isMarked
                                   ? Icons.bookmark
                                   : Icons.bookmark_border,
-                              color: Colors.grey[700],
+                              color: Colors.black54,
                             ),
                             onPressed: () {
                               handleBookmarkTapped();
