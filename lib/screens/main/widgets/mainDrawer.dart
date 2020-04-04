3521a301eb13599093947d6d55fbd20276cde3a8
Alexander Teusz <teusz.alexander@gmail.com>
2020-04-04 15:16:17 +0200

    added expansiontile and removed text from drawerheader

--- lib/screens/main/widgets/mainDrawer.dart
+++ lib/screens/main/widgets/mainDrawer.dart
@@ -18,64 +18,111 @@ class MainDrawer extends StatelessWidget {
         clipper: OvalRightBorderClipper(),
         child: Drawer(
             child: Container(
-      color: Colors.white,
-      child: Column(
-        children: <Widget>[
-          DrawerHeader(
-            child: Column(
-              children: <Widget>[
-                Image(image: AssetImage('assets/images/logo.png'), width: 150),
-                Text(
-                  'Menu',
-                  style: TextStyle(fontSize: 20),
-                )
-              ],
-            ),
-          ),
-          Expanded(
-              child: ListView(
-            padding: EdgeInsets.zero,
+          color: Colors.white,
+          child: Column(
             children: <Widget>[
-              ListTile(
-                leading: Icon(Icons.school),
-                title: Text('Language Tests'),
+              DrawerHeader(
+                child: Image(image: AssetImage('assets/images/logo.png'), width: 150),
               ),
-            ],
-          )),
-          Container(
-              child: Align(
-                  alignment: FractionalOffset.bottomCenter,
-                  child: Container(
-                      child: Column(
+              Expanded(
+                  child: ListView(
+                padding: EdgeInsets.zero,
+                children: <Widget>[
+                  ExpansionTile(
+                    leading: Icon(
+                      Icons.school,
+                      color: Colors.grey[700],
+                    ),
+                    trailing: Icon(
+                      Icons.keyboard_arrow_down,
+                      color: Colors.grey[700],
+                    ),
+                    title: Text('Language Tests',
+                        style: TextStyle(
+                          color: Colors.grey[900],
+                        )),
                     children: <Widget>[
-                      Divider(),
                       ListTile(
-                          leading: Icon(Icons.help),
-                          title: Text('Help'),
-                          onTap: () => _launchUrl(
-                              'https://github.com/elaisasearch/elaisa.org/blob/master/README.md')),
-                      Divider(),
-                      ListTile(
-                          leading: Icon(Icons.code),
-                          title: Text('See Code'),
-                          onTap: () => _launchUrl(
-                              'https://github.com/elaisasearch/app')),
-                      ListTile(
-                        leading: Icon(Icons.bug_report),
-                        title: Text('Report Bug'),
-                        onTap: () => _launchUrl(
-                            'https://github.com/elaisasearch/app/issues'),
+                        leading: Text(
+                          '🇩🇪',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        title: Text(
+                          'Deutsch',
+                          style: TextStyle(fontSize: 15),
+                        ),
+                        onTap: () {
+                          _launchUrl(
+                              'https://sprachtest.de/einstufungstest-deutsch');
+                        },
                       ),
-                      Divider(thickness: 1),
-                      Container(
-                          padding: EdgeInsets.all(10),
-                          height: 50,
-                          child: Text('v1.0.0 - elaisa.org',
-                              style: TextStyle(fontWeight: FontWeight.bold)))
+                      ListTile(
+                        leading: Text(
+                          '🇬🇧',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        title: Text(
+                          'English',
+                          style: TextStyle(fontSize: 15),
+                        ),
+                        onTap: () {
+                          _launchUrl(
+                              'https://sprachtest.de/einstufungstest-englisch');
+                        },
+                      ),
+                      ListTile(
+                        leading: Text(
+                          '🇪🇸',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        title: Text(
+                          'Español',
+                          style: TextStyle(fontSize: 15),
+                        ),
+                        onTap: () {
+                          _launchUrl(
+                              'https://sprachtest.de/einstufungstest-spanisch');
+                        },
+                      )
                     ],
-                  ))))
-        ],
-      ),
-    )));
+                  ),
+                ],
+              )),
+              Container(
+                  child: Align(
+                      alignment: FractionalOffset.bottomCenter,
+                      child: Container(
+                          child: Column(
+                        children: <Widget>[
+                          Divider(),
+                          ListTile(
+                              leading: Icon(Icons.help),
+                              title: Text('Help'),
+                              onTap: () => _launchUrl(
+                                  'https://github.com/elaisasearch/elaisa.org/blob/master/README.md')),
+                          Divider(),
+                          ListTile(
+                              leading: Icon(Icons.code),
+                              title: Text('See Code'),
+                              onTap: () => _launchUrl(
+                                  'https://github.com/elaisasearch/app')),
+                          ListTile(
+                            leading: Icon(Icons.bug_report),
+                            title: Text('Report Bug'),
+                            onTap: () => _launchUrl(
+                                'https://github.com/elaisasearch/app/issues'),
+                          ),
+                          Divider(thickness: 1),
+                          Container(
+                              padding: EdgeInsets.all(10),
+                              height: 50,
+                              child: Text('v1.0.0 - elaisa.org',
+                                  style:
+                                      TextStyle(fontWeight: FontWeight.bold)))
+                        ],
+                      ))))
+            ],
+          ),
+        )));
   }
 }
